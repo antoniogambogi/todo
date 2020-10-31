9439187299c8cca2a815bb3769623ef09c0def7f
antoniogambogi <antoniogambogi88@gmail.com>
2020-10-31 15:38:42 -0300

    Implementando formulário de todo

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
   <div class="" id="app">
     <div class="container grid-xs py-2">
       <img class="img-responsive img-logo" src="@/assets/logo.png" alt="logo Vue">
-      
+      <form @submit.prevent="addTodo(todo)">
+        <div class="input-group">
+          <input type="text" v-model="todo.description" class="form-input" placeholder="Novo todo">
+          <button class="btn btn-primary input-group-btn">Adicionar</button>
+        </div>
+        {{ todos }}
+      </form>
     </div>
   </div>
 </template>
@@ -10,12 +16,21 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {todos: [], todo: { checked: false } };
+  },
+  methods: {
+    addTodo(todo) {
+      todo.id = Date.now()
+      this.todos.push(todo)
+    }
+  }
 };
 </script>
 
 <style scoped>
 .img-logo {
   max-width: 100px;
-  margin: 0 auto;
+  margin: 20px auto 20px auto;
 }
 </style>
