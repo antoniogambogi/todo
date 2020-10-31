877c50066d20621cb7dde8348a7cc62c56983887
antoniogambogi <antoniogambogi88@gmail.com>
2020-10-31 17:50:41 -0300

    Adicionando a opção de remover Todo

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
         </div>
       </form>
       <div class="todo-list">
-        <todo v-for="t in todos" :key="t.id" @toggle="toggleTodo" :todo="t" />
+        <todo v-for="t in todos" :key="t.id" @toggle="toggleTodo" @remove="removeTodo" :todo="t" />
       </div>
     </div>
   </div>
@@ -35,6 +35,13 @@ export default {
       if (index > -1) {
         const checked = !this.todos[index].checked;
         this.$set(this.todos, index, {...this.todos[index], checked });
+      }
+    },
+
+    removeTodo(todo) {
+      const index = this.todos.findIndex(item => item.id === todo.id);
+      if (index > -1) {
+        this.$delete(this.todos, index);
       }
     }
   }
