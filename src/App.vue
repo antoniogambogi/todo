93f22d356c8ec642c29ba109e47391d9c7393c20
antoniogambogi <antoniogambogi88@gmail.com>
2020-10-31 15:05:16 -0300

    Instalando o Spectre.css

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
 <template>
-  <div id="app">Hello Vue</div>
+  <div class="" id="app">
+    <div class="container grid-xs py-2">
+      Hello Vue
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-}
+  name: "App",
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
